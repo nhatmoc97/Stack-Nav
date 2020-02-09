df6b92093953f551e7eed3e51c66985ffa57fcd4
nhatmoc97 <nhatmoc97@gmail.com>
2020-02-09 15:40:48 +0700

    Add Stack-Nav folder

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+const renderGridItem = (itemdata) => {
+  return <View style={styles.gridItem}>
+    <Text>{itemdata.item.title}</Text>
+  </View>;
+}
+const CategoriesScreen = props => {
+  return (
+    <FlatList keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2} />
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
+  }
+});
+
+export default CategoriesScreen;
