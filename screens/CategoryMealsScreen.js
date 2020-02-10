c665af58e55981901a40a37abb13b4b8d88bd8f7
nhatmoc97 <nhatmoc97@gmail.com>
2020-02-10 23:33:58 +0700

    Pull Stack-Nav folder

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 import { CATEGORIES, Meal, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 const CategoryMealScreen = props => {
   const renderMealItem = itemData => {
-    return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
-    );
+    return <MealItem
+      title={itemData.item.title}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      affordability={itemData.item.affordability}
+      image={itemData.item.imageUrl}
+      onSelectMeal={() => {
+        props.navigation.navigate({
+          routeName: 'MealDetail', params: {
+            mealId: itemData.item.id
+          }
+        });
+      }} />;
   };
   const catId = props.navigation.getParam('categoryId');
   const displayedMeal = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0
@@ -17,6 +26,7 @@ const CategoryMealScreen = props => {
       <FlatList data={displayedMeal}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   );
@@ -33,7 +43,8 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 15
   }
 });
 
